479e256420b29e4dd56989a03396ad7417135811
romantuze <romantuze@gmail.com>
2021-08-29 08:15:31 +0400

    fixed bags

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,10 @@ import React, {useEffect, useState, useMemo} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-
 type Props = {
   todos?: TodoItem[];
 };
 
-// type TodoItem =
 interface  TodoItem  { 
   id: string | number;
   title: string;
@@ -21,137 +18,91 @@ interface AppProps {
 
 const App: React.FC<AppProps> = ({todosProp}) => {
 
-  const [todos, setTodos] = useState<TodoItem[]>([]); // props.todos
-
-
+  const [todos, setTodos] = useState<TodoItem[]>([]); 
   const [filter, setFilter] = useState<'all' | 'undone'>('all');
   const [doneCount, setDoneCount] = useState(0);
 
-  const [filtrTodos,setFiltrTodos] = useState<TodoItem[]>([]); 
-
- /* const getFiltrTodos = () => {
-    let newTodos = todos.filter((todo) => (filter === 'undone' ? !todo.isDone : false));
-    if (filter === 'undone') return newTodos;
-    return todos;
-  }*/
-  
-
-
-  useEffect(() => {   
-    
-    if (todosProp) { // props.todos
+  useEffect(() => {       
+    if (todosProp) { 
       setFilter('all');
       setTodos(todosProp)
     }
-
-
   },[]);
 
   const filteredTodos = useMemo(() => {
+    if (filter==='all') return todos;
     return todos.filter((todo) => (filter === 'undone' ? !todo.isDone : false));
   },[filter,todos]);
 
-  function addTodo ()  { // title?: string
-    /*let title: string = "";*/
+  const computedDoneCount = useMemo(() => {
+    return todos.filter((todo) => (filter === 'undone' ? true : false)).length;
+  },[todos]);
 
-    //if (!title) {
-     
-    //}
+  function addTodo ()  { 
     let title = prompt('What to do?'); 
-
-    if (title) {
-      
+    if (title) {      
       let newTodo = {
-        id: +Date.now(), // Date.now
+        id: Date.now(), 
         title,
         isDone: false,
       };
-
       setTodos([...todos, newTodo]);
-    }
-
-    
+    }    
   };
 
-
-
-
   const markAsDone = (todo: TodoItem) => {
-    /*let id = todo.id;
-    let index = todos.indexOf(id);*/
     let index = todos.findIndex((i) =>  i.id === todo.id);
-
     if (index >= 0) {
-      //let newTodos = todos.splice(index, 1);
-      let newTodos = todos.filter((i) => i.id !== index);
+      let newTodos = todos.slice();
+      newTodos[index].isDone = true;
       setTodos(newTodos);
     }
-
-    setDoneCount(todos.length);
+    let done = doneCount + 1;
+    setDoneCount(done);
   };
 
   const markAsUndone = (todo: TodoItem) =>  {
-    const index = todos.slice().findIndex((item) => todo.id);
-
+    let index = todos.findIndex((i) =>  i.id === todo.id);
     if (index >= 0) {
-
-      //todos.splice(index, 1, {...todo, isDone: false});
-      let newTodos = todos.filter((i) => i.id !== index);
-      newTodos.splice(index, 1, {...todo, isDone: false});
-
-      setTodos([...newTodos]);
+      let newTodos = [...todos];
+      newTodos[index].isDone = false;
+      setTodos(newTodos);
     }
-
-    let done = todos.length - 1;
+    let done = (doneCount > 0) ? (doneCount - 1) : 0;
     setDoneCount(done);
-
   };
 
   const deleteTodo = (todo: TodoItem) => {
-
     let index = todos.findIndex((i) =>  i.id === todo.id);
-
-    //const index = todos.indexOf(todo.id);
-
     if (index >= 0) {
-      //todos.splice(index, 1);
       let newTodos = todos.filter((i) => i.id !== todo.id);
-
       setTodos(newTodos);
     }
+    let done = (todo.isDone === true) ? (doneCount - 1) : doneCount;
+    setDoneCount(done);
   };
 
   const onFilterButtonClick = (e: any) => {
     e.preventDefault();
     if (filter === 'all') {
       setFilter('undone');
-  }
+    }
     else {
       setFilter('all');
     }
-
-
-   
-
-    //setFilter((filter === 'all') ? 'all' : 'undone');
   };
 
-
-
-
-//todos.count
   return (
     <div>
-      <p>{`${doneCount} / ${todos.length}`}</p> 
+      <p>{ `${doneCount} / ${todos.length}`}</p> 
       <ul>
         {filteredTodos
-          //.filter((todo) => (filter === 'undone' ? !todo.isDone : false))
           .map((todo) => (
             <li key={todo.id}>
               <p>{`${todo.isDone ? '✅ ' : ''}${todo.title}`}</p>
               <button
                 onClick={() => {
-                  todo.isDone ? markAsDone(todo) : markAsUndone(todo);
+                  todo.isDone ? markAsUndone(todo) : markAsDone(todo);
                 }}>
                 {'Done'}
               </button>
@@ -160,8 +111,7 @@ const App: React.FC<AppProps> = ({todosProp}) => {
           ))}
       </ul>
       <button onClick={() => addTodo()}>{'Add'}</button>
-      <button onClick={(e) => onFilterButtonClick(e)}>
-        
+      <button onClick={(e) => onFilterButtonClick(e)}>        
         {`Show ${filter === 'all' ? 'undone' : 'all'} todos`}
       </button>
       <ExpensiveTree />
@@ -171,11 +121,9 @@ const App: React.FC<AppProps> = ({todosProp}) => {
 
 function ExpensiveTree() {
   let now = performance.now();
-
   while (performance.now() - now < 1000) {
     // Artificial delay -- do nothing for 1000ms
   }
-
   return null;
 }
 
